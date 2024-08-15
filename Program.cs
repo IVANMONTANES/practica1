562b1ee5ab9a359a0f705460882b68b66abab073
ivan montanes <ivanmontanesfilipit@gmail.com>
2024-08-14 21:59:20 -0300

    segunda calculadora

--- Program.cs
+++ Program.cs
@@ -1,57 +1,46 @@
 ﻿bool seguir = true;
-while(seguir){
-    Console.WriteLine("1 sumar");
-    Console.WriteLine("2 restar");
-    Console.WriteLine("3 multiplicar");
-    Console.WriteLine("4 dividir");
-    Console.WriteLine("5 salir");
-    string elegirCadena = Console.ReadLine();
-    int elegir = 0;
-    if(int.TryParse(elegirCadena,out elegir)){
-        if(elegir >=1 && elegir <= 4){
-
-            double numero1 = 0,numero2 = 0;
-            Console.WriteLine("ingrese el primer numero:");
-            string numero1Cadena = Console.ReadLine();
-            while(!double.TryParse(numero1Cadena,out numero1)){
-                Console.WriteLine("no se ingreso un numero valido");
-                Console.WriteLine("ingrese el primer numero:");
-                numero1Cadena = Console.ReadLine();
-            }
-
-            Console.WriteLine("ingres el segundo numero:");
-            string numero2Cadena = Console.ReadLine();
-            while(!double.TryParse(numero2Cadena,out numero2) || numero2Cadena == "0"){
-                if(numero2Cadena == "0"){
-                    Console.WriteLine("no se puede dividir en 0");
-                    Console.WriteLine("ingrese el segundo numero:");
-                    numero2Cadena = Console.ReadLine();
-                }else{
-                    Console.WriteLine("no se ingreso un numero valido");
-                    Console.WriteLine("ingrese el segundo numero:");
-                    numero2Cadena = Console.ReadLine();
-                }
-            }
-            double resultado = 0;
+do{
+Console.WriteLine("1 valor absoluto");
+Console.WriteLine("2 cuadrado");
+Console.WriteLine("3 raiz cuadrada");
+Console.WriteLine("4 seno");
+Console.WriteLine("5 coseno");
+Console.WriteLine("6 parte entera");
+Console.WriteLine("7 salir");
+Console.WriteLine("eliga una opcion:");
+string elegirCadena = Console.ReadLine();
+int elegir;
+if(int.TryParse(elegirCadena,out elegir)){
+    if(elegir >= 1 && elegir <= 6){
+        Console.WriteLine("ingrese un numero");
+        string numeroCadena = Console.ReadLine();
+        double numero = 0,resultadoDecimal = 0;
+        int resultadoEntero = 0;
+        if(double.TryParse(numeroCadena, out numero)){
             switch(elegir){
-                case 1: resultado = numero1+numero2; break;
-                case 2: resultado = numero1-numero2; break;
-                case 3: resultado = numero1*numero2; break;
-                case 4: resultado = numero1/numero2; break;
+                case 1: resultadoDecimal = Math.Abs(numero); break;
+                case 2: resultadoDecimal = Math.Pow(numero,2); break;
+                case 3: resultadoDecimal = Math.Sqrt(numero); break;
+                case 4: resultadoDecimal = Math.Sin(numero); break;
+                case 5: resultadoDecimal = Math.Cos(numero); break;
+                case 6: resultadoEntero = (int)numero; break;
             }
-            Console.WriteLine("resultado: "+resultado);
+            if(elegir >= 1 && elegir <= 5){
+                Console.WriteLine("resultado: "+resultadoDecimal);
+            }else{
+                Console.WriteLine("resultado: "+resultadoEntero);
+            }
         }
         else{
-            if(elegir == 5){
-                seguir = false;
-                Console.WriteLine("saliendo...");
-            }
-            else{
-                Console.WriteLine("no se eligio una opcion valida");
-            }
+            Console.WriteLine("no se indico un numero valido");
         }
     }
+    else if(elegir == 7){
+        seguir = false;
+        Console.WriteLine("saliendo...");
+    }
     else{
-        Console.WriteLine("no se indico un numero");
+        Console.WriteLine("no se indico una opcion valida");
     }
 }
+}while(seguir);
